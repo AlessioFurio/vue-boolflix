854ad3eaddfcac80518e3152728818f53d58c469
Furio Alessio <alefurio@hotmail.it>
2020-11-24 17:25:09 +0100

    aggiunta ricerca serie tv

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue({
 
 	data: {
 		movies: [], // array dei movies
+		series: [],
 		search: '', // input v-model
 		stars: [1,2,3,4,5],
 		imgFlags: [
@@ -28,12 +29,26 @@ var app = new Vue({
 				this.movies = risposta.data.results; // assegno ad array movies la risposta API
 
 				console.log(this.movies);
-
-
-
 			}); // fine then
-		} // fine searcFilm
+		}, // fine searcFilm
 
+		searchSeries(){
+
+			axios
+			.get('https://api.themoviedb.org/3/search/tv', {
+				params: {
+					api_key: '3b8fd41bd03a505fc96b3ca3fb875ed4',
+					query: this.search
+				}
+				})
+			.then((risposta2) =>{
+
+				this.series = risposta2.data.results; // assegno ad array movies la risposta API
+
+				console.log(this.series);
+			}); // fine then
+
+		}// fine searchSeries
 	}, // fine methods
 
 	mounted() {
