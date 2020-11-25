ce74a017aa11b96d0a5b261bb1591a459eb878c0
Furio Alessio <alefurio@hotmail.it>
2020-11-25 01:15:47 +0100

    un po' di css

--- main.js
+++ main.js
@@ -7,21 +7,17 @@ var app = new Vue({
 		movies: [], // array dei movies
 		series: [],
 		search: '', // input v-model
-		stars: [1,2,3,4,5],
-		imgFlags: [
-			"ita.jpg",
-			"en.jpg"
-		],
+		stars: [1,2,3,4,5], // array x stampare stelle
 	},
 
 	methods: {
 
-		searchFilm(){
+		searchFilm(){ // funzione cerca film
 			axios
 			.get('https://api.themoviedb.org/3/search/movie', {
 				params: {
 					api_key: '3b8fd41bd03a505fc96b3ca3fb875ed4',
-					query: this.search
+					query: this.search // assegno come query il valore dell' input utente nel v model
 				}
 				})
 			.then((risposta) =>{
@@ -32,7 +28,7 @@ var app = new Vue({
 			}); // fine then
 		}, // fine searcFilm
 
-		searchSeries(){
+		searchSeries(){ // funzione cerca series
 
 			axios
 			.get('https://api.themoviedb.org/3/search/tv', {
@@ -43,7 +39,7 @@ var app = new Vue({
 				})
 			.then((risposta2) =>{
 
-				this.series = risposta2.data.results; // assegno ad array movies la risposta API
+				this.series = risposta2.data.results; // assegno ad array series la risposta API
 
 				console.log(this.series);
 			}); // fine then
